5e136aad0d0b02172782495dcf8eb3ff9d7dd267
Pavel Kurmyza <tmrrwnxtsn@gmail.com>
2022-01-28 13:28:50 +0400

    Add closing the database connections

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
 	"github.com/ulstu-schedule/bot-telegram/internal/config"
 	"github.com/ulstu-schedule/bot-telegram/internal/store/postgres"
 	"github.com/ulstu-schedule/bot-telegram/internal/telegram"
@@ -19,12 +20,24 @@ func Run(configPath string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer func(db *sqlx.DB) {
+		err = db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(studentDB)
 	studentStore := postgres.NewStudentStore(studentDB)
 
 	scheduleDB, err := postgres.NewDB(cfg.ScheduleDatabaseURL)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer func(db *sqlx.DB) {
+		err = db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(scheduleDB)
 	scheduleStore := postgres.NewScheduleStore(scheduleDB)
 
 	api, err := tgbotapi.NewBotAPI(cfg.Token)
